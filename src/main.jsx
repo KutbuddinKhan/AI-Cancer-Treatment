a91837ee46f669350ab91b8a84822d24192495c5
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2024-08-25 01:06:15 +0530

    installation done

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import ReactDom from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+
+import App from "./App";
+import "./index.css";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(
+  <PrivyProvider
+    appId="cm08ir0wt0423rzudld449dbb"
+    config={{
+      loginMethods: ["email", "wallet"],
+      appearance: {
+        theme: "dark",
+      },
+      embeddedWallets: {
+        createOnLogin: "users-without-wallets",
+      },
+    }}
+  >
+    <Router>
+      <App />
+    </Router>
+  </PrivyProvider>,
+);
