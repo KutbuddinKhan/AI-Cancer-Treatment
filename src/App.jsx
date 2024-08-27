08aa1b9448eb5f657618710978ed34d2b87d1d93
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2024-08-28 00:21:35 +0530

    Record page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import { Route, Routes, useNavigate} from 'react-router-dom'
+import { Route, Routes, useNavigate } from "react-router-dom";
+
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import { Onboarding } from "./pages";
+
+import { Home, Profile, Onboarding} from './pages'
+import MedicalRecord from './pages/records/index'
+
+import { useStateContext } from "./context";
+import { usePrivy } from "@privy-io/react-auth";
 
 
 const App = () => {
-    return (
-        <div className="flex relative min-h-screen flex-row bg-[#13131a]">
-            <div className="relative mr-10 hidden sm:flex">
-                {/* sidebar */}
-                <Sidebar />
+  const navigate = useNavigate();
+  const { currentUser } = useStateContext();
 
-            </div>
+  const { user, authenticated, ready, login } = usePrivy();
 
-            <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
-                {/* Navbar */}
-                <Navbar />
-                
-                <Routes>
-                    <Route path="/" element={ <Home /> } />
-                    <Route path="/onboarding" element={ <Onboarding /> } />
-                </Routes>
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    if (ready && !authenticated) {
+      login();
+    } else if (user && !currentUser) {
+      navigate("/onboarding");
+    }
+  }, [ready, currentUser, navigate]);
+
+
+
+  return (
+    <div className="relative flex min-h-screen flex-row bg-[#13131a]">
+      <div className="relative mr-10 hidden sm:flex">
+        {/* sidebar */}
+        <Sidebar />
+      </div>
+
+      <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
+        {/* Navbar */}
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/medical-records" element={<MedicalRecord />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default App;
